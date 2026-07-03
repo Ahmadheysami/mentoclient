da5ed60b9dc8e7dd6b78157c56084a567c6cbfad
ahmad heysami <91902295+Ahmadheysami@users.noreply.github.com>
2026-07-03 07:14:08 -0700

    use from sonner toaster

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@ import {Toaster} from "vue-sonner"
 
 <template>
   <UApp>
-    <Toaster :dir="'rtl'" :class="'font-yekan-regular! text-13'" :theme="'light'" :position="'top-center'" rich-colors :gap="10" />
+    <Toaster :dir="'rtl'" :class="'font-yekan-light! text-13'" :theme="'light'" :position="'top-center'" rich-colors :gap="10" />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
