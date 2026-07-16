8bb3d7e0c0b7610f7dbf8aee5bf01a12d3073b86
ahmad heysami <91902295+Ahmadheysami@users.noreply.github.com>
2026-07-16 04:37:38 -0700

    get notification stats

--- app/components/App/TopBar.vue
+++ app/components/App/TopBar.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 const user = useUser(),
+    notification = useNotification(),
     fullName = `${user.state.user?.fullName.first || 'کاربر'} ${user.state.user?.fullName.last || 'عادی'}`
+
+onMounted(async () => {
+    await notification.get({public: '1'})
+})
 </script>
 
 <template>
@@ -19,7 +24,7 @@ const user = useUser(),
             <ULink
                 to="/notifications"
                 class="bg-white w-13 hover:bg-white/90 h-13 shadow-inner shadow-x-primary-400 grid place-items-center rounded-full">
-                <UChip :ui="{ base: 'bg-red-500' }" size="xs" position="top-left">
+                <UChip :show="Number(notification.state.notificationStats?.unread) > 0" :ui="{ base: 'bg-red-500' }" size="xs" position="top-left">
                     <UIcon name="solar:bell-outline" size="26" />
                 </UChip>
             </ULink>
