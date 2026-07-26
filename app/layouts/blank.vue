eafc92a55ba82803d99f51ec57e45104872568ff
ahmad heysami <91902295+Ahmadheysami@users.noreply.github.com>
2026-07-26 03:22:12 -0700

    remove pb-33 from blank layout

--- app/layouts/blank.vue
+++ app/layouts/blank.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="default__layout custom-scroll bg-x-primary-content-100 max-w-md border-x border-slate-200 h-dvh overflow-y-auto mx-auto font-yekan-regular pb-33">
+    <div class="relative default__layout custom-scroll bg-x-primary-content-100 max-w-md border-x border-slate-200 h-dvh overflow-y-auto mx-auto font-yekan-regular ">
         <slot />
     </div>
 </template>
