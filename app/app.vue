6a7ab773a102b44763a54e1cb35c451e21ece4b1
ahmad heysami <91902295+Ahmadheysami@users.noreply.github.com>
2026-09-24 11:33:24 -0700

    debug: splash screen and formatting

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Hooks
+const nuxtApp = useNuxtApp(),
+  splashState = ref<boolean>(true),
+  loadPageHandler = () => {
+    setTimeout(() => {
+      splashState.value = false;
+    }, 2250);
+  };
+nuxtApp.hooks.hook("app:mounted", loadPageHandler);
+nuxtApp.hooks.hook("page:loading:end", loadPageHandler);
+</script>
 
 <template>
   <UApp>
+    <AppSplashScreen v-if="splashState" />
     <ClientOnly>
       <NotificationHandler />
-      <Toaster :dir="'rtl'" :class="'font-yekan-light! text-13 z-9999999'" :theme="'light'" :position="'top-center'" rich-colors :gap="10" />
+      <Toaster
+        :dir="'rtl'"
+        :class="'font-yekan-light! text-13 z-9999999'"
+        :theme="'light'"
+        :position="'top-center'"
+        rich-colors
+        :gap="10"
+      />
     </ClientOnly>
     <NuxtLayout>
       <NuxtPage />
